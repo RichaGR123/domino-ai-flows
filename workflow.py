2312267b70f2763bc7cac3da493a8712bda9e814
Richa Gupta <richa.gupta@capgemini.com>
2025-01-28 00:34:16 -0800

    Pushed from Domino: https://siwrkshp.partner-engineering-team-sandbox.domino.tech/workspace/richa-gupta/TrialAITrainingFlowTemplate1?executionId=679333ec78246f4de800b229

--- workflow.py
+++ workflow.py
@@ -11,7 +11,7 @@ data_merge_task = DominoJobTask(
     name="merge_data",
     domino_job_config=DominoJobConfig(
         #Command="python /mnt/code/scripts/merge-data.py {inputs.data_patha} {inputs.data_pathb}"
-        Command="python /mnt/code/scripts/merge-data.py {data_patha} {data_pathb}"
+        Command="python /mnt/code/scripts/merge-data.py "/mnt/code/data/datasetA.csv" "/mnt/code/data/datasetB.csv""
     ),
     environment_name="Domino Standard Environment Py3.11 R4.4",
     hardware_tier_name="Small",
@@ -61,7 +61,8 @@ def training_workflow() -> FlyteFile:
     
      # Run the data Merge task
     #data_merge_results = data_merge_task(data_patha=data_patha, data_pathb=data_pathb)
-    data_merge_results = data_merge_task(data_patha="/mnt/code/data/datasetA.csv", data_pathb="/mnt/code/data/datasetB.csv")
+    #data_merge_results = data_merge_task(data_patha="/mnt/code/data/datasetA.csv", data_pathb="/mnt/code/data/datasetB.csv")
+    data_merge_results = data_merge_task()
     
     # Run the data preparation task
     #data_prep_results = data_prep_task(data_path=data_path)
