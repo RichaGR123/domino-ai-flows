a3644317f7720aecb10395dbf2f5fdf277fba276
Richa Gupta <richa.gupta@capgemini.com>
2025-01-28 06:13:51 -0800

    Pushed from Domino: https://siwrkshp.partner-engineering-team-sandbox.domino.tech/workspace/richa-gupta/TrialAITrainingFlowTemplate1?executionId=679333ec78246f4de800b229

--- scripts/merge-data.py
+++ scripts/merge-data.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pandas as pd
 
@@ -6,21 +7,33 @@ datasetA_path = sys.argv[1]
 datasetB_path = sys.argv[2]
 
 # Load data
-print(f"Loading dataset A from: {datasetA_path}")
+#print(f"Loading dataset A from: {datasetA_path}")
 a = pd.read_csv(datasetA_path, index_col='Id')
 
-print(f"Loading dataset B from: {datasetB_path}")
+#print(f"Loading dataset B from: {datasetB_path}")
 b = pd.read_csv(datasetB_path, index_col='Id')
 
-print(f"Received dataset A path: {datasetA_path}")
-print(f"Received dataset B path: {datasetB_path}")
+#print(f"Received dataset A path: {datasetA_path}")
+#print(f"Received dataset B path: {datasetB_path}")
 
 # Merge data
 print('Merging data...')
 merged = pd.concat([a, b], axis=0).reset_index(drop=True)
 
+
+# Ensure output directory exists
+output_dir = '/mnt/code/outputs/'
+os.makedirs(output_dir, exist_ok=True)
+
 # Write output
-#output_path = '/workflow/outputs/merged_data.csv'
-output_path = '/mnt/code/outputs/merged_data.csv'
+output_path = os.path.join(output_dir, 'merged_data.csv')
 merged.to_csv(output_path, index=False)
 print(f"Merged data saved to: {output_path}")
+
+#
+
+# Write output
+##output_path = '/workflow/outputs/merged_data.csv'
+#output_path = '/mnt/code/outputs/merged_data.csv'
+#merged.to_csv(output_path, index=False)
+#print(f"Merged data saved to: {output_path}")
