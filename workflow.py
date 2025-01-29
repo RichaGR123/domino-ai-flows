3225518d284a1ee3f85ab2388bdd566c1c43a8de
Richa Gupta <richa.gupta@capgemini.com>
2025-01-28 21:41:33 -0800

    Pushed from Domino: https://siwrkshp.partner-engineering-team-sandbox.domino.tech/workspace/richa-gupta/TrialAITrainingFlowTemplate1?executionId=679333ec78246f4de800b229

--- workflow.py
+++ workflow.py
@@ -19,9 +19,6 @@ data_load_taskA = DominoJobTask(
         "data_patha": str,
     },
     
-    #inputs={
-        #"data_patha": FlyteFile["csv"],
-    #},
     outputs={"datasetA": FlyteFile["csv"]},
     use_latest=True
 )
@@ -38,9 +35,6 @@ data_load_taskB = DominoJobTask(
         "data_pathb": str,
     },
     
-    #inputs={
-        #"data_pathb": FlyteFile["csv"],
-    #},
     outputs={"datasetB": FlyteFile["csv"]},
     use_latest=True
 )
@@ -49,22 +43,10 @@ data_merge_task = DominoJobTask(
     name="merge_data",
     domino_job_config=DominoJobConfig(
          Command="python /mnt/code/scripts/merge-data.py {datasetA} {datasetB}",
-        #Command="python /mnt/code/scripts/merge-data.py {.inputs.data_patha} {.inputs.data_pathb}",
-        #Command="python /mnt/code/scripts/merge-data.py {data_patha} {data_pathb}",
-        #Command="python /mnt/code/scripts/merge-data.py "/mnt/code/data/datasetA.csv" "/mnt/code/data/datasetB.csv""
     ),
     environment_name="Domino Standard Environment Py3.11 R4.4",
     hardware_tier_name="Small",
     
-    #inputs={
-        #"data_patha": str,
-        #"data_pathb": str,
-    #},
-    
-    #inputs={
-        #"data_patha": FlyteFile["csv"],
-        #"data_pathb": FlyteFile["csv"],
-    #},
      inputs={
         "datasetA": FlyteFile["csv"],
         "datasetB": FlyteFile["csv"],
@@ -80,12 +62,11 @@ data_prep_task = DominoJobTask(
     domino_job_config=DominoJobConfig(Command="python /mnt/code/scripts/process-data.py {merged_data}"),
     environment_name="Domino Standard Environment Py3.11 R4.4",
     hardware_tier_name="Small",
-    #inputs={"data_path": str},
     inputs={
         "merged_data": FlyteFile["csv"],
     },
     outputs={"processed_data": FlyteFile["csv"]},
-    #use_project_defaults_for_omitted=True,
+   
     use_latest=True
 )
 
@@ -100,24 +81,18 @@ training_task = DominoJobTask(
         "batch_size": int,
     },
     outputs={"model": FlyteFile},
-    #use_project_defaults_for_omitted=True,
     use_latest=True
 )
 
 # Define the workflow
 @workflow
-#def training_workflow() -> FlyteFile:
-#def training_workflow(data_patha: FlyteFile["csv"], data_pathb: FlyteFile["csv"]) -> FlyteFile:
 def training_workflow(data_patha: str, data_pathb: str) -> FlyteFile:
-#def training_workflow(data_patha="/mnt/data/datasetA.csv", data_pathb="/mnt/data/datasetB.csv") -> FlyteFile:
     
     #data_load_taskA
     data_load_A = data_load_taskA(data_patha=data_patha)
     data_load_B = data_load_taskB(data_pathb=data_pathb)
      # Run the data Merge task
     data_merge_results = data_merge_task(datasetA=data_load_A, datasetB=data_load_B)
-    #data_merge_results = data_merge_task(data_patha="/mnt/code/data/datasetA.csv", data_pathb="/mnt/code/data/datasetB.csv")
-    #data_merge_results = data_merge_task()
     
     # Run the data preparation task
     #data_prep_results = data_prep_task(data_path=data_path)
