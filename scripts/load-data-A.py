b1450c561de0c211222bbb789d501f5356a01297
Richa Gupta <richa.gupta@capgemini.com>
2025-01-28 23:26:49 -0800

    Pushed from Domino: https://siwrkshp.partner-engineering-team-sandbox.domino.tech/workspace/richa-gupta/TrialAITrainingFlowTemplate1?executionId=679333ec78246f4de800b229

--- scripts/load-data-A.py
+++ scripts/load-data-A.py
@@ -7,10 +7,14 @@ import sys
 #input_location = f"/workflow/inputs/{input_name}"
 
 datasetA_path = sys.argv[1]
-print("this is system argument",datasetA_path)
+print("this is system argument A",datasetA_path)
+print("these are all args", sys.argv)
 
-datasetA_path = "/mnt/code/data/datasetA.csv"
-print("This is hard coded path",datasetA_path)
+#datasetB_path = sys.argv[1]
+#print("this is system argument B",datasetB_path)
+
+#datasetA_path = "/mnt/code/data/datasetA.csv"
+#print("This is hard coded path",datasetA_path)
 
 with open(datasetA_path, "r") as file:
     input_csv = file.read()
