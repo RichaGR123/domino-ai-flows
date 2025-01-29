42ceea2f303eff554305ab64a2c8526c7db36a85
Richa Gupta <richa.gupta@capgemini.com>
2025-01-29 01:54:02 -0800

    Pushed from Domino: https://siwrkshp.partner-engineering-team-sandbox.domino.tech/workspace/richa-gupta/TrialAITrainingFlowTemplate1?executionId=679333ec78246f4de800b229

--- workflow.py
+++ workflow.py
@@ -10,9 +10,9 @@ from flytekit.types.file import FlyteFile
 data_load_taskA = DominoJobTask(
     name='Load_Data_A',
     domino_job_config=DominoJobConfig(
-         Command='python /mnt/code/scripts/load-data-A.py {data_patha}',
+         #Command='python /mnt/code/scripts/load-data-A.py {data_patha}',
          #Command='python /mnt/code/scripts/load-data-A.py {sys.argv[1]}',
-         #Command='python /mnt/code/scripts/load-data-A.py /mnt/code/data/datasetA.csv',
+         Command=f'python /mnt/code/scripts/load-data-A.py /mnt/code/data/datasetA.csv',
     ),
     environment_name="Domino Standard Environment Py3.11 R4.4",
     hardware_tier_name="Small",
@@ -94,15 +94,15 @@ training_task = DominoJobTask(
 
 # Define the workflow
 @workflow
-def training_workflow(data_patha: str, data_pathb: str) -> FlyteFile:
+#def training_workflow(data_patha: str, data_pathb: str) -> FlyteFile:
 
 #uncomment below while hardcoding
-#def training_workflow(data_pathb: str) -> FlyteFile: 
+def training_workflow(data_pathb: str) -> FlyteFile: 
     
     #data_load_taskA
-    data_load_A = data_load_taskA(data_patha=data_patha)
+    #data_load_A = data_load_taskA(data_patha=data_patha)
     #uncomment below while hardcoding
-    #data_load_A = data_load_taskA()
+    data_load_A = data_load_taskA()
     data_load_B = data_load_taskB(data_pathb=data_pathb)
     
      # Run the data Merge task
